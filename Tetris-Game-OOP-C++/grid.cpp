57d2a929dcfbfad46620f3a0c917702f23425242
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-06-08 21:11:18 +0100

    set up the game loop + start creating the grid

---
+++ Tetris-Game-OOP-C++/grid.cpp
@@ -0,0 +1,8 @@
+//
+//  grid.cpp
+//  Tetris-Game-OOP-C++
+//
+//  Created by Sofiane Medhouny Laouina on 08/06/2024.
+//
+
+#include "grid.hpp"
---
+++ Tetris-Game-OOP-C++/grid.hpp
@@ -0,0 +1,13 @@
+//
+//  grid.hpp
+//  Tetris-Game-OOP-C++
+//
+//  Created by Sofiane Medhouny Laouina on 08/06/2024.
+//
+
+#ifndef grid_hpp
+#define grid_hpp
+
+#include <stdio.h>
+
+#endif /* grid_hpp */
