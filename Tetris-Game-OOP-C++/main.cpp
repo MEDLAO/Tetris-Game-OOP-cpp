9ecc20c4dfed58e2944c8c101ada8035f1ff9520
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-06-08 18:12:32 +0100

    add Raylib

--- Tetris-Game-OOP-C++/main.cpp
+++ Tetris-Game-OOP-C++/main.cpp
@@ -2,13 +2,28 @@
 //  main.cpp
 //  Tetris-Game-OOP-C++
 //
-//  Created by Sofiane Medhouny Laouina on 07/06/2024.
+//  Created by MEDLAO on 07/06/2024.
 //
 
 #include <iostream>
+#include "raylib.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    
+    InitWindow(screenWidth, screenHeight, "Raylib Example");
+    
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+    
+    CloseWindow();
+    
     return 0;
 }
