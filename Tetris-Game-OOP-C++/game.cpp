1f06205d60670113d8c717b57e56602ea7070f66
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-06-11 16:48:58 +0100

    complete code to move the blocks

---
+++ Tetris-Game-OOP-C++/game.cpp
@@ -0,0 +1,8 @@
+//
+//  game.cpp
+//  Tetris-Game-OOP-C++
+//
+//  Created by Sofiane Medhouny Laouina on 11/06/2024.
+//
+
+#include "game.hpp"
---
+++ Tetris-Game-OOP-C++/game.hpp
@@ -0,0 +1,13 @@
+//
+//  game.hpp
+//  Tetris-Game-OOP-C++
+//
+//  Created by Sofiane Medhouny Laouina on 11/06/2024.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <stdio.h>
+
+#endif /* game_hpp */
